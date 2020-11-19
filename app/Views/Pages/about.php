7f51aab87b10553f5f314117fe46ed82548841c8
Ahmad Nurjati <anurjati16@gmail.com>
2020-11-19 21:40:44 +0700

    Episode 7

---
+++ app/Views/Pages/about.php
@@ -0,0 +1,12 @@
+<?= $this->extend('layout/template'); ?>
+
+<?= $this->section('content'); ?>
+<div class="container">
+    <div class="row">
+        <div class="col">
+            <h1><?= $title; ?></h1>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Repellendus quia numquam laborum alias, excepturi quidem harum laudantium cum eaque sapiente, modi temporibus ipsa earum recusandae error! Labore sequi odio perferendis!</p>
+        </div>
+    </div>
+</div>
+<?= $this->endSection('content'); ?>
