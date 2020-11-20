1c6ef93b49395db3d4c143ac401d8ce4ea0853a2
Ahmad Nurjati <anurjati16@gmail.com>
2020-11-20 12:32:46 +0700

    Episode 10 and few Fixes

--- app/Controllers/Komik.php
+++ app/Controllers/Komik.php
@@ -32,6 +32,36 @@ class Komik extends BaseController
             'title' => 'Detail Komik',
             'komik' => $this->komikModel->getKomik($slug)
         ];
+
+        // jika komik tidak ada di tabel
+        if (empty($data['komik'])) {
+            throw new \CodeIgniter\Exceptions\PageNotFoundException('Judul komik ' . $slug . ' tidak ditemukan');
+        }
         return view('komik/detail', $data);
+    }
+
+    public function create()
+    {
+        $data = [
+            'title' => 'Form Tambah Data Komik'
+        ];
+        return view('komik/create', $data);
+    }
+
+    public function save()
+    {
+        //dd($this->request->getVar('judul'));
+        $slug = url_title($this->request->getVar('judul'), '-', true);
+        $this->komikModel->save([
+            'judul' => $this->request->getVar('judul'),
+            'slug' => $slug,
+            'penulis' => $this->request->getVar('penulis'),
+            'penerbit' => $this->request->getVar('penerbit'),
+            'sampul' => $this->request->getVar('sampul')
+        ]);
+
+        session()->setFlashdata('pesan', 'Data berhasil ditambahkan.');
+
+        return redirect()->to('/komik');
     }
 }
